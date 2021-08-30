5853b97ed434264fa91773704d66913bc03be91c
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-08-30 20:45:09 +0200

    tested

---
+++ .terraform.lock.hcl
@@ -0,0 +1,22 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/aviatrixsystems/aviatrix" {
+  version = "2.20.0"
+  hashes = [
+    "h1:5QTvAt7+HBase8Kt2SxFIivYmnEc5TYhqH4edNy4owM=",
+    "zh:11c15dc7d12c0ce9a4ce882d8d50759d21af20d0dafede08c07be4f82b71cb89",
+    "zh:28988f0aa6da6b880487d5a484a9cc0c345e368ef55a2b41ee4b8898c7419e72",
+    "zh:2e32e6396e0b2bd5f0c390121e8237d14c308336ebd5a7844e212649cf9abe49",
+    "zh:3c67dc3240fe62a5ee960e783ec63dc2c34af93394adadeadfbe68c2ad40f3cb",
+    "zh:3e1bab358c5e6ed3e14d69bed71922525bf5f4300b833d118f113ea08d463f09",
+    "zh:4bff584aa1f5ee3095db1cc140127ea972c40ffa9077939773366a7e75e58d1e",
+    "zh:51c003b72cd84777b2bd9076cef404e03fbc912602e742e799736a842a10f535",
+    "zh:7ba9ad84b1d6f0d8ec8507b166cb1d86687c76d7dc4404c554c28d88e760b71f",
+    "zh:89c2edd0d2caf6372a5a92d1e621c6931ee7ba53ebeaa4fd0c555a02312e1e08",
+    "zh:bc35c334ccd8ab7f5a1bfc131635561ee9d1861e30fe7851fbd85f42b12c6cb9",
+    "zh:c844dfbf98c97096732d801a5435f802f12756640cdceeb18abe73fa59f31982",
+    "zh:d68c61058868452399716dfd2b03524bb0236c4bdc47d7abfa8bc5665a623cbe",
+    "zh:f6a87e7a7a5ef0aeec5136a3dd67af014343f9af9dc3ca10e65315d3b8a0ee1d",
+  ]
+}
--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@
 terraform {
   required_providers {
     aviatrix = {
-      source  = "hashicorp/aviatrix"
-      version = "~> 2.19.0"
+      source = "aviatrixsystems/aviatrix"
     }
   }
+  required_version = ">= 0.13"
 }
 
 # Configure Aviatrix provider
@@ -23,10 +23,50 @@ provider "aviatrix" {
 # Create an AWS VPC
 resource "aviatrix_vpc" "AVFFSP16" {
   cloud_type           = 1
-  account_name         = "devops"
+  account_name         = "Eskimoo"
   region               = "eu-central-1"
   name                 = "AVFFSP16"
   cidr                 = "10.77.16.0/20"
   aviatrix_transit_vpc = false
   aviatrix_firenet_vpc = false
+}
+
+# Create an AWS HUB VPC
+resource "aviatrix_vpc" "AVFFHUB" {
+  cloud_type           = 1
+  account_name         = "Eskimoo"
+  region               = "eu-central-1"
+  name                 = "AVFFHUB"
+  cidr                 = "10.77.0.0/20"
+  aviatrix_transit_vpc = true
+  aviatrix_firenet_vpc = false
+}
+
+# Create an Aviatrix AWS Gateway SP16
+resource "aviatrix_gateway" "gw_for_sp16" {
+  cloud_type   = 1
+  account_name = "Eskimoo"
+  gw_name      = "GWFFSP16"
+  vpc_id       = "vpc-0f1867c3e96718043"
+  vpc_reg      = "eu-central-1"
+  gw_size      = "t2.micro"
+  subnet       = "10.77.19.0/24"
+  
+  
+}
+
+# Create an Aviatrix AWS Transit Network Gateway
+resource "aviatrix_transit_gateway" "gw_for_hub" {
+  cloud_type               = 1
+  account_name             = "Eskimoo"
+  gw_name                  = "transit"
+  vpc_id                   = "vpc-07496a4c111cbf324"
+  vpc_reg                  = "eu-central-1"
+  gw_size                  = "t2.micro"
+  subnet                   = "10.77.0.80/28"
+  ha_subnet                = "10.77.0.112/28"
+  ha_gw_size               = "t2.micro"
+  enable_active_mesh       = true
+  enable_hybrid_connection = true
+  connected_transit        = true
 }
