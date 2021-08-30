1783ef33f79ae5e5a25b44153a59a2f34985d510
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-08-30 12:13:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,26 @@
+
+
+
+
 # Configure Aviatrix provider
 provider "aviatrix" {
-  controller_ip           = "1.2.3.4"
+  controller_ip           = "35.162.98.189"
   username                = "admin"
-  password                = "password"
+  password                = "Ccie16661!"
   skip_version_validation = false
   version                 = "2.19.0"
 }
 
+
+
+
 # Create an AWS VPC
-resource "aviatrix_vpc" "aws_vpc" {
+resource "aviatrix_vpc" "AVFFSP16" {
   cloud_type           = 1
   account_name         = "devops"
-  region               = "us-west-1"
-  name                 = "aws-vpc"
-  cidr                 = "10.0.0.0/16"
+  region               = "eu-central-1"
+  name                 = "AVFFSP16"
+  cidr                 = "10.77.16.0/20"
   aviatrix_transit_vpc = false
   aviatrix_firenet_vpc = false
 }
